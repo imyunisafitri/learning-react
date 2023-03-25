d1467c2d6ed57c4376dd9619b5ab93dfb19ef8c4
Yuu code <yunisafitrix@gmail.com>
2023-03-25 15:22:59 +0700

    day-3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+//import App from "./App";
+import LocalStorage from "./LocalStorage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <LocalStorage/>
   </React.StrictMode>
 );
 
